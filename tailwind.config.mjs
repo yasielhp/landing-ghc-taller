fe7877436452357acaa6fab81c368b53a32204e2
Yasiel Hernández Portal <yasielhp@gmail.com>
2024-05-20 14:30:22 +0100

    chore: Update Astro configuration and dependencies

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#E900E7",
+        },
+        background: {
+          DEFAULT: "#030929",
+        },
+      },
+    },
+  },
+  plugins: [],
 }
